16ef7c4ef8b35ee0de12df3d5bd0afa387f78b5f
zoejingwen <jshen9613@gmail.com>
2025-01-13 14:29:24 +0100

    corrige

--- dist_levenshtein.go
+++ dist_levenshtein.go
@@ -52,11 +52,13 @@ func pourcentage(a string, b string, dist int) float64 {
 	a_rune := []rune(a)
 	b_rune := []rune(b)
 	n, m := len(a_rune), len(b_rune)
-	res = float64(dist) / float64(max(n, m))
+	res = float64(dist) / float64(max(n, m)) * 100
+	return res
 }
 
 func main() {
-	a := ""
-	b := "chats"
+	a := "ogiyowiiiigg <,?[!12]"
+	b := "ogiyowiiiggs <,?[!12]"
 	fmt.Println(dist_lev(a, b))
+	fmt.Printf("la différence entre les deux chaînes est %v %%\n", pourcentage(a, b, dist_lev(a, b)))
 }
