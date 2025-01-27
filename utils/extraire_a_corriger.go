0929ea00819f79a603dec2b0124590ea44ece112
abracadamain <paulineigue@gmail.com>
2025-01-27 14:54:01 +0100

    supprime les print de test

--- utils/extraire_a_corriger.go
+++ utils/extraire_a_corriger.go
@@ -1,15 +1,12 @@
 package utils
 
 import (
-	"fmt"
 	"strings"
 	"unicode"
 )
 
 func Txt_to_liste(txt_a_corriger string) []string {
 	txt_a_corriger = strings.ToLower(txt_a_corriger)
-	fmt.Print(txt_a_corriger)
-
 	// On remplace la ponctuation par des espaces
 	var sb strings.Builder
 	for _, r := range txt_a_corriger {
@@ -19,13 +16,10 @@ func Txt_to_liste(txt_a_corriger string) []string {
 			sb.WriteRune(r)
 		}
 	}
-	fmt.Print(txt_a_corriger)
 	txt_a_corriger = sb.String()
-	fmt.Print(txt_a_corriger)
 
 	// On sépare le texte en mots
 	liste := strings.Fields(txt_a_corriger)
-	fmt.Print(txt_a_corriger)
 
 	return liste
 }
