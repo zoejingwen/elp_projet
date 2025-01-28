36341a25887a16a103195aae0c1ae70ffd6d25e1
abracadamain <163309324+abracadamain@users.noreply.github.com>
2025-01-28 15:50:41 +0100

    orga dossiers

--- ELM/ELM/main/main.elm
+++ ELM/ELM/main/main.elm
@@ -4,6 +4,7 @@ import Browser
 import Html exposing (Html, Attribute, button, div, input, text)
 import Html.Attributes exposing (..)
 import Html.events exposing (onClick, onInput)
+import Parser exposing (..)
 
 -- MAIN
 main =
