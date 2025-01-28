999825f38e71d72937876d1b0c6c55a0463d3954
abracadamain <163309324+abracadamain@users.noreply.github.com>
2025-01-28 17:07:00 +0100

    orga fichier et feuille de style

---
+++ Elm/src/Main.elm
@@ -0,0 +1,44 @@
+module Main exposing (..)
+
+import Browser
+import Html exposing (Html, Attribute, button, div, input, text)
+import Html.Attributes exposing (..)
+import Html.events exposing (onClick, onInput)
+import ../Parser/TcTurtleParser exposing (..)
+
+-- MAIN
+
+main =
+    Browser.sandbox { init = init, update = update, view = view }
+
+-- MODEL
+
+type alias Model =
+    { commandes : String
+    }
+
+init : Model
+init =
+    Model ""
+
+-- UPDATE
+
+type Msg
+    = Start 
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Start -> model -- display model.commandes -- adapter display
+
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ text "Type in your TcTurtle code below :"
+        , input [ placeholder "exemple : [Repeat 360 [ Right 1, Forward 1]]", value model.commandes ]
+        , button [ onClick Start] [ text "Draw"]
+        ] -- ajouter le display svg
