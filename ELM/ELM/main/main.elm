2d610fbd8e2d064654ae1b14865af305781b21db
abracadamain <163309324+abracadamain@users.noreply.github.com>
2025-01-28 16:01:52 +0100

    création model

--- ELM/ELM/main/main.elm
+++ ELM/ELM/main/main.elm
@@ -4,13 +4,19 @@ import Browser
 import Html exposing (Html, Attribute, button, div, input, text)
 import Html.Attributes exposing (..)
 import Html.events exposing (onClick, onInput)
-import Parser exposing (..)
+import TcTurtleParser exposing (..)
 
 -- MAIN
+
 main =
     Browser.sandbox { init = init, update = update, view = view }
 
 -- MODEL
 
+type alias Model =
+    { commandes : String
+    , bouton : Int
+    , dessin : Svg
+    }
 
 -- UPDATE
