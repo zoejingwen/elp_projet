a428bbbd3c8e1aeb76919a01180db31e8df0700b
zoejingwen <jshen9613@gmail.com>
2025-02-06 18:34:56 +0100

    reponse

---
+++ JavaScript/reponse.js
@@ -0,0 +1,21 @@
+const readline = require('readline-sync');
+
+function reponse(secret_word) {
+    let reponse = readline.question("Tu penses que cela doit être quel mot ? ");
+    let comparaison;
+
+    if (reponse !== null && reponse !== "") {
+        if (reponse === secret_word) {
+            comparaison = "réussite";
+        } else {
+            comparaison = "échec";
+        }
+    } else {
+        comparaison = "pass";
+    }
+
+    return comparaison;
+}
+
+// Exemple d'utilisation
+console.log(reponse("chat"));
